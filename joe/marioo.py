0b7ccdd821603e0aac3ddb6a571d62a2bad47a75
MTDropH <m.teare@aol.com>
2025-06-28 13:23:40 +0100

    Refactor game initialization and player mechanics; add water projectile functionality

--- joe/marioo.py
+++ joe/marioo.py
@@ -5,10 +5,69 @@ pygame.mixer.init()
 pygame.mixer.music.load("Jack/sounds/8bit-sample-69080.mp3")
 pygame.mixer.music.play(-1)
 
-water_images = [
-    pygame.image.load('joe/images/water1.jpg').convert_alpha(),
-    pygame.image.load('joe/images/water2.jpg').convert_alpha(),
+WIDTH, HEIGHT = 800, 448
+FPS = 60
+GRAVITY = 0.5
+PLAYER_SPEED = 4
+JUMP_VELOCITY = -10
+TILE = 32
+LEVEL_WIDTH = 4096
+
+SKY      = (135, 206, 235)
+GROUND   = (160, 82, 45)
+PLAYER_C = (255, 0, 0)
+ENEMY_C  = (0, 0, 255)
+PLAT_C   = (124, 252, 0)
+FLAG_C   = (255, 215, 0)
+
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Running Man")
+clock = pygame.time.Clock()
+
+background_img_raw = pygame.image.load("joe/images/streetbackround.jpg").convert()
+background_img = pygame.transform.scale(background_img_raw, (
+    int(background_img_raw.get_width() * (HEIGHT / background_img_raw.get_height())), HEIGHT))
+background_width = background_img.get_width()
+background_scroll_speed = 0.5
+
+idle_images = [
+    pygame.transform.scale(pygame.image.load('joe/images/idle1RM.png').convert_alpha(), (TILE, int(TILE * 3))),
+    pygame.transform.scale(pygame.image.load('joe/images/idle2RM.png').convert_alpha(), (TILE, int(TILE * 3)))
 ]
+
+run_images = [
+    pygame.transform.scale(pygame.image.load('joe/images/running1RM.png').convert_alpha(), (TILE, int(TILE * 3))),
+    pygame.transform.scale(pygame.image.load('joe/images/running2RM.png').convert_alpha(), (TILE, int(TILE * 3)))
+]
+
+class Entity(pygame.sprite.Sprite):
+    def __init__(self, x, y, w, h, colour):
+        super().__init__()
+        self.image = pygame.Surface((w, h))
+        self.image.fill(colour)
+        self.rect = self.image.get_rect(topleft=(x, y))
+        self.vel = pygame.Vector2(0, 0)
+
+    def update(self):
+        self.rect.x += self.vel.x
+        self.rect.y += self.vel.y
+
+WATER_SIZE = TILE // 1        # 16 px if TILE is 32; tweak as you like (e.g. TILE//3)
+
+water_images = [
+    pygame.transform.scale(
+        pygame.image.load('joe/images/water1.jpg').convert_alpha(),
+        (WATER_SIZE, WATER_SIZE)
+    ),
+    pygame.transform.scale(
+        pygame.image.load('joe/images/water2.jpg').convert_alpha(),
+        (WATER_SIZE, WATER_SIZE)
+    ),
+]
+
+water_group = pygame.sprite.Group()
+
 jump_sound = pygame.mixer.Sound("Jack/sounds/sword_whoosh.mp3")
 
 # Rest of the code remains the same...
@@ -26,9 +85,10 @@ class Player(Entity):
         self.image = self.idle_images[0]
         self.rect = self.image.get_rect(topleft=(x, y))
         self.on_ground = False
-        self.last_shoot = 0
-        self.shot_cooldown = 500  # milliseconds
-        self.facing_right = True
+        
+        self.last_shoot     = 0           # NEW
+        self.shot_cooldown  = 400         # ms   – NEW
+        self.facing_right   = True        # NEW
 
     def handle_input(self, keys):
         self.vel.x = 0
@@ -39,7 +99,7 @@ class Player(Entity):
         if (keys[pygame.K_SPACE] or keys[pygame.K_UP]) and self.on_ground:
             self.vel.y = JUMP_VELOCITY
             jump_sound.play()
-
+        
         if keys[pygame.K_f] and pygame.time.get_ticks() - self.last_shoot > self.shot_cooldown:
             direction = 1 if self.facing_right else -1
             stream = WaterProjectile(self.rect.centerx, self.rect.centery, direction)
@@ -52,20 +112,43 @@ class Player(Entity):
             self.vel.y = TILE
 
     def collide(self, tiles):
-        return
+        self.rect.x += self.vel.x
+        hits = [t for t in tiles if self.rect.colliderect(t)]
+        for tile in hits:
+            if self.vel.x > 0:
+                self.rect.right = tile.left
+            elif self.vel.x < 0:
+                self.rect.left = tile.right
+
+        self.rect.y += self.vel.y
+        hits = [t for t in tiles if self.rect.colliderect(t)]
+        self.on_ground = False
+        for tile in hits:
+            if self.vel.y > 0:
+                self.rect.bottom = tile.top
+                self.vel.y = 0
+                self.on_ground = True
+            elif self.vel.y < 0:
+                self.rect.top = tile.bottom
+                self.vel.y = 0
+
     def update(self):
-        
+        # Choose which animation to use
+        if self.vel.x != 0:
+            self.current_images = self.run_images
+        else:
+            self.current_images = self.idle_images
 
-        water_group.update()
+        # Update animation frame
+        self.animation_timer += self.animation_speed
+        if self.animation_timer >= 1:
+            self.animation_timer = 0
+            self.frame_index = (self.frame_index + 1) % len(self.current_images)
 
-        for stream in water_group:
-            for enemy in enemies:
-                if stream.rect.colliderect(enemy.rect):
-                    enemy.kill()
-                    stream.kill()
+        self.image = self.current_images[self.frame_index]
+        self.rect = self.image.get_rect(topleft=self.rect.topleft)
 
-for stream in water_group:
-    screen.blit(stream.image, stream.rect.move(-camera_x, 0))
+        super().update()
 
 enemy_images_right = [
     pygame.transform.scale(pygame.image.load('joe/images/badGuy1.png').convert_alpha(), (TILE, TILE*1.5)),
@@ -110,6 +193,35 @@ class Enemy(Entity):
 
         self.image = images[self.frame_index]
         self.rect = self.image.get_rect(topleft=self.rect.topleft)
+
+class WaterProjectile(pygame.sprite.Sprite):
+    SPEED = 7          # pixels per frame
+    ANIM_SPEED = 0.2   # how fast the water animates
+
+    def __init__(self, x, y, direction):
+        super().__init__()
+        self.images = water_images
+        self.frame_index = 0
+        self.animation_timer = 0
+
+        self.image = self.images[0]
+        self.rect = self.image.get_rect(center=(x, y))
+        self.vel = pygame.Vector2(self.SPEED * direction, 0)
+
+    def update(self):
+        # Move
+        self.rect.x += self.vel.x
+
+        # Animate
+        self.animation_timer += self.ANIM_SPEED
+        if self.animation_timer >= 1:
+            self.animation_timer = 0
+            self.frame_index = (self.frame_index + 1) % len(self.images)
+            self.image = self.images[self.frame_index]
+
+        # Kill if it leaves the level bounds
+        if self.rect.right < 0 or self.rect.left > LEVEL_WIDTH:
+            self.kill()
 
 import json
 
@@ -174,6 +286,13 @@ def main():
         player.collide(tiles)
 
         sprites.update()
+        water_group.update()                       # NEW
+
+        # --- Projectile ⇆ Enemy collisions -------------------------------
+        for stream in water_group:
+            hit_list = pygame.sprite.spritecollide(stream, enemies, dokill=True)
+            if hit_list:
+                stream.kill()
 
         for enemy in enemies:
             if player.rect.colliderect(enemy.rect):
@@ -203,6 +322,9 @@ def main():
         draw_tiles(screen, tiles, camera_x)
         for sprite in sprites:
             screen.blit(sprite.image, sprite.rect.move(-camera_x, 0))
+            
+        for stream in water_group:                 # NEW
+            screen.blit(stream.image, stream.rect.move(-camera_x, 0))
 
         pygame.draw.rect(screen, FLAG_C, flag.move(-camera_x, 0))
 
